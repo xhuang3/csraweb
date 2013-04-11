1e7d640209f702e5c641ed95b9df03a3410b96c8
SimbaHuang <xhuang3@binghamton.edu>
2013-04-11 00:35:58 -0400

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CsraWeb::Application.config.secret_token = '3857345e3389aaf2bbc7249ca0e1c2549b2d2b01d845ff5b1e7b64730960c00bea7120990af1a4b638734e070d24665b430d4b437feef56843cb6e0a3007fbad'
